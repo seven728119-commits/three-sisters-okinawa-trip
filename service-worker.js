8abf04a0a13cd5a0da548495b209a98299ef9ac6
seven728119-commits <seven728119@gmail.com>
2026-09-04 23:07:52 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "three-sisters-okinawa-";
-const CACHE_NAME = `${CACHE_PREFIX}20260904-v28`;
+const CACHE_NAME = `${CACHE_PREFIX}20260904-v35`;
 const APP_SHELL = [
   "./",
   "./index.html",
